56280f70d389dc13a320e420903470d47f42b45c
Stephen Christopher Adams <vengeanz@gmail.com>
2017-03-17 15:15:37 +1300

    Configured ESLint

--- Problem1/Solution1.js
+++ Problem1/Solution1.js
@@ -1,16 +1,13 @@
-/* jshint esversion: 6 */
-/* jshint node: true */
-"use strict";
+'use strict';
 
 /*
 Recovery Key: 576451-2JY2RH7Aq5xiHSKHm22X6VyLXcAEICNZirOOn1YJ
    Generated: Tue, 27 Dec 2016, 11:54.18
    */
 
-function getNumbers(lowerBound, upperBound)
-{
+function getNumbers(lowerBound, upperBound) {
     let numbers = [];
-    for (let i = lowerBound; i < upperBound; i++){
+    for (let i = lowerBound; i < upperBound; i++) {
         numbers.push(i);
     }
 
@@ -21,18 +18,21 @@ function divisibleBy(...divisors)
 {
     return function(value)
     {
-        return divisors.some(element => value % element === 0);
+        return divisors.some((element) => value % element === 0);
     };
 }
 
-let divisibleBy3or5 = divisibleBy(3,5);
+let divisibleBy3or5 = divisibleBy(3, 5);
 
 
-console.log("Project Euler: Problem 1.\n\nIf we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.\nThe sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.\n");
+let problemDescription = 'Project Euler: Problem 1.\n\nIf we list all the natural numbers below 10 that are multiples of 3 or 5 we get 3, 5, 6 and 9.';
+problemDescription += '\nThe sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.\n';
 
-let originalList = getNumbers(1,1000);
+console.log(problemDescription);
 
-let filteredList = originalList.filter(divisibleBy3or5).reduce((a,b) => a + b );
+let originalList = getNumbers(1, 1000);
+
+let filteredList = originalList.filter(divisibleBy3or5).reduce((a, b) => a + b );
 
 console.log(filteredList);
 
